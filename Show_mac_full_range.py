95b55f7b7cc24aaf47d86af52a41c9c764005e60
Łukasz <79469737+IamLukeInIT@users.noreply.github.com>
2022-06-27 22:14:59 +0200

    Create Show_mac_full_range.py

---
+++ Show_mac_full_range.py
@@ -0,0 +1,23 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+sw_1 = {
+    'device_type': 'cisco_ios',
+    'host':   '10.10.10.10',
+    'username': 'username',
+    'password': getpass(),
+}
+
+cmd = "show mac address-table int"
+interfaces = [f'g1/0/{i}' for i in range (1,49)]
+
+with ConnectHandler(**sw_1) as net_connect:
+    for interface in interfaces:
+        output = net_connect.send_command(cmd + interface)
+
+        print()
+        print(output)
+        print()
+
+net_connect.disconnect()
+print("CLose session")
